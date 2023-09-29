460aa139e08a2f404be31394eed7cab77abc9af3
Combir <144319259+Combir@users.noreply.github.com>
2023-09-29 22:27:19 +0300

    replace emoticon

---
+++ replace emoticon.py
@@ -0,0 +1,11 @@
+# Take user input for the sentence
+input_sentence = input("Enter a sentence: ")
+
+# Take user input for the character to replace
+character_to_replace = input("Enter the character to replace: ")
+
+# Replace all occurrences of the character with '😎'
+output_sentence = input_sentence.replace(character_to_replace, '😎')
+
+# Print the updated sentence
+print("Updated sentence:", output_sentence)
