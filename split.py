a37fdd4cfeeff90268a809c6d445eb5a9ccde67a
Combir <144319259+Combir@users.noreply.github.com>
2023-09-29 22:13:25 +0300

    Create split.py

---
+++ split.py
@@ -0,0 +1,9 @@
+python
+# Take user input for the sentence
+input_sentence = input("Enter a sentence: ").split()
+
+# Count the number of words
+word_count = len(input_sentence)
+
+# Print the word count
+print("Number of words:", word_count)
